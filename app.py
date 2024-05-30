5ef826a8cb3883ab1ee77705c3f5982e39eb4ba9
abdelahy <abdelhay.github@gmail.com>
2024-05-30 13:32:05 +0100

    version2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import nltk
-nltk.download('popular',quiet=True)
+nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
@@ -9,8 +9,6 @@ from keras.models import load_model
 model = load_model('model.h5')
 import json
 import random
-
-
 intents = json.loads(open('data.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))

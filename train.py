028bd025c046be83b9c3fb2015766cfc2ea12fa4
abdelahy <abdelhay.github@gmail.com>
2024-06-23 11:45:09 +0100

    just an update

--- train.py
+++ train.py
@@ -81,7 +81,8 @@ train_x = [training[x][0] for x in range(len(training))]
 train_y = [training[x][1] for x in range(len(training))]
 print("Training data created")
 
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output
+# layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
@@ -90,7 +91,8 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient 
+# gives good results for this model
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
